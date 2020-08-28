420b41f72a1d2d33f57d645952f74c3e918d6c6c
Thongchai Kitiyanantawong <mypumz@gmail.com>
2020-08-29 00:53:11 +0700

    refactor: separate icon

--- src/components/BookmarkButton/BookmarkButton.tsx
+++ src/components/BookmarkButton/BookmarkButton.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
-
 import styled from '@emotion/styled'
 
-import { Icon } from '@/components/uikit'
-
+import { BookmarkOffIcon, BookmarkIcon } from './BookmarkIcon'
 interface BookmarkButtonProps {
   text?: string
   isActive?: boolean
@@ -20,38 +18,18 @@ function BookmarkButton(props: BookmarkButtonProps) {
   )
 }
 
-export const BookmarkIcon = () => {
-  return (
-    <Icon
-      iconSize={16}
-      name="BookmarkIcon"
-      iconPath="M18,1.14H6A2.44,2.44,0,0,0,3.55,3.55V22.86L12,19.24l8.45,3.62V3.55A2.44,2.44,0,0,0,18,1.14Z"
-    />
-  )
-}
-
-export const BookmarkOffIcon = () => {
-  return (
-    <Icon
-      iconSize={16}
-      name="BookmarkOffIcon"
-      iconPath="M18,1.14H6A2.42,2.42,0,0,0,3.54,3.55V22.86L12,19.24l8.44,3.62V3.55A2.42,2.42,0,0,0,18,1.14Zm0,18.1-6-2.63L6,19.24V4.76A1.22,1.22,0,0,1,7.16,3.55h9.65A1.21,1.21,0,0,1,18,4.76Z"
-    />
-  )
-}
-
 const BookmarkStyled = styled.div`
   background-color: var(--color-primary);
   border-radius: 5px;
   color: #fff;
   text-transform: uppercase;
-  cursor: pointer;
   border: none;
   appearance: none;
   padding: var(--space3);
   font-size: 14px;
   display: inline-flex;
   align-items: center;
+  cursor: pointer;
 
   &:hover {
     background-color: var(--color-primary-light);
